ad945eb2f22fd1a1d3c5d838ddff1be1652db701
DODI Software <support@dodisoftware.com>
2022-02-23 22:07:03 -0500

    Initial Commit

---
+++ AGI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AGI
+//
+//  Created by Phil Inglis on 2022-02-23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
