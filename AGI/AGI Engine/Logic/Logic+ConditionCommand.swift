49015d361c0b5431913b36c903223220df885b91
DODI Software <support@dodisoftware.com>
2022-05-05 22:39:48 -0400

    Support more Condition Commands and Unit Tests for all conditions

--- AGI/AGI Engine/Logic/Logic+ConditionCommand.swift
+++ AGI/AGI Engine/Logic/Logic+ConditionCommand.swift
@@ -43,7 +43,7 @@ extension Logic {
             
             // equal
             if name == CommandName.condition_equal {
-                guard data.count == 2, (0 ... 255).contains(data[0]), (0 ... 255).contains(data[1]) else { return false }
+                guard dataIsValid(bytes: 2) else { return false }
                 
                 let variableNum = Int(data[0])
                 let value = data[1]
@@ -51,28 +51,164 @@ extension Logic {
                 return Logic.variables[variableNum] == value
             }
             
-            // equal.v
-            if name == CommandName.condition_equal_v {
-                guard data.count == 2, (0 ... 255).contains(data[0]), (0 ... 255).contains(data[1]) else { return false }
+            // equal_v
+            else if name == CommandName.condition_equal_v {
+                guard dataIsValid(bytes: 2) else { return false }
+                
+                let variableNum1 = Int(data[0])
+                let variableNum2 = Int(data[1])
+                
+                return Logic.variables[variableNum1] == Logic.variables[variableNum2]
+            }
+            
+            // less
+            else if name == CommandName.condition_less {
+                guard dataIsValid(bytes: 2) else { return false }
                 
                 let variableNum = Int(data[0])
-                let valueNum = Int(data[1])
+                let value = data[1]
                 
-                return Logic.variables[variableNum] == Logic.variables[valueNum]
+                return Logic.variables[variableNum] < value
+            }
+            
+            // less_v
+            else if name == CommandName.condition_less_v {
+                guard dataIsValid(bytes: 2) else { return false }
+                
+                let variableNum1 = Int(data[0])
+                let variableNum2 = Int(data[1])
+                
+                return Logic.variables[variableNum1] < Logic.variables[variableNum2]
+            }
+            
+            // greater
+            else if name == CommandName.condition_greater {
+                guard dataIsValid(bytes: 2) else { return false }
+                
+                let variableNum = Int(data[0])
+                let value = data[1]
+                
+                return Logic.variables[variableNum] > value
+            }
+            
+            // greater_v
+            else if name == CommandName.condition_greater_v {
+                guard dataIsValid(bytes: 2) else { return false }
+                
+                let variableNum1 = Int(data[0])
+                let variableNum2 = Int(data[1])
+                
+                return Logic.variables[variableNum1] > Logic.variables[variableNum2]
             }
             
             // isset
-            if name == CommandName.condition_isset {
-                guard data.count == 1, (0 ... 255).contains(data[0]) else { return false }
+            else if name == CommandName.condition_isset {
+                guard dataIsValid(bytes: 1) else { return false }
                 
                 return Logic.flags[Int(data[0])] == true
             }
             
             // isset_v
-            if name == CommandName.condition_isset_v {
-                guard data.count == 1, (0 ... 255).contains(data[0]) else { return false }
+            else if name == CommandName.condition_isset_v {
+                guard dataIsValid(bytes: 1) else { return false }
                 
                 return Logic.variables[Int(data[0])] == 1
+            }
+            
+            // position
+            else if name == CommandName.condition_position {
+                guard dataIsValid(bytes: 5) else { return false }
+                
+                let object = screenObjects[Int(data[0])]
+                let testMinX = data[1]
+                let testMinY = data[2]
+                let testMaxX = data[3]
+                let testMaxY = data[4]
+                
+                let objectInX = (testMinX ..< testMaxX).contains(UInt8(object.posX))
+                let objectInY = (testMinY ..< testMaxY).contains(UInt8(object.posY))
+                
+                return objectInX && objectInY
+            }
+            
+            // compare_strings
+            else if name == CommandName.condition_compare_strings {
+                guard dataIsValid(bytes: 2), data[0] < Logic.numStrings, data[1] < Logic.numStrings else { return false }
+                
+                let ignoredCharacters: Set<Character> = [Character(String(format: "%c", 0x20)),
+                                                         Character(String(format: "%c", 0x09)),
+                                                         "-",
+                                                         ".",
+                                                         ",",
+                                                         ":",
+                                                         ";",
+                                                         "!",
+                                                         "\\"]
+                
+                let stringNum1 = Int(data[0])
+                let stringNum2 = Int(data[1])
+                
+                // Remove characters that are ignored and convert to lowercase
+                var string1 = Logic.strings[stringNum1].lowercased()
+                string1.removeAll(where: { ignoredCharacters.contains($0) } )
+                
+                var string2 = Logic.strings[stringNum2].lowercased()
+                string2.removeAll(where: { ignoredCharacters.contains($0) } )
+                
+                return string1.compare(string2) == .orderedSame
+            }
+            
+            // obj_in_box
+            else if name == CommandName.condition_obj_in_box {
+                guard dataIsValid(bytes: 5) else { return false }
+                
+                let object = screenObjects[Int(data[0])]
+                let testMinX = data[1]
+                let testMinY = data[2]
+                let testMaxX = data[3]
+                let testMaxY = data[4]
+                
+                return object.posX >= testMinX &&
+                    object.posY >= testMinY &&
+                    object.posX + object.sizeX <= testMaxX &&
+                    object.posY + object.sizeY <= testMaxY
+            }
+            
+            // center_position
+            else if name == CommandName.condition_center_position {
+                guard dataIsValid(bytes: 5) else { return false }
+                
+                let object = screenObjects[Int(data[0])]
+                let testMinX = data[1]
+                let testMinY = data[2]
+                let testMaxX = data[3]
+                let testMaxY = data[4]
+                
+                return object.posX + object.sizeX / 2 >= testMinX &&
+                    object.posX + object.sizeX / 2 <= testMaxX &&
+                    object.posY >= testMinY &&
+                    object.posY <= testMaxY
+            }
+            
+            // right_position
+            else if name == CommandName.condition_right_position {
+                guard dataIsValid(bytes: 5) else { return false }
+                
+                let object = screenObjects[Int(data[0])]
+                let testMinX = data[1]
+                let testMinY = data[2]
+                let testMaxX = data[3]
+                let testMaxY = data[4]
+                
+                return object.posX + object.sizeX >= testMinX &&
+                    object.posX + object.sizeX <= testMaxX &&
+                    object.posY >= testMinY &&
+                    object.posY <= testMaxY
+            }
+            
+            // in_motion_using_mouse
+            else if name == CommandName.condition_in_motion_using_mouse {
+                return false
             }
             
             return false
---
+++ AGITests/LogicConditionTests.swift
@@ -0,0 +1,540 @@
+//
+//  LogicConditionTests.swift
+//  AGITests
+//
+//  Created by Phil Inglis on 2022-05-03.
+//
+
+import XCTest
+@testable import AGI
+
+class LogicConditionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+        // Reset all variables and flags
+        for index in 0 ..< Logic.numVariables {
+            Logic.variables[index] = 0
+        }
+        
+        for index in 0 ..< Logic.numFlags {
+            Logic.flags[index] = false
+        }
+        
+        // Reset all object position and size
+        for index in 0 ..< Logic.numScreenObjects {
+            Logic.screenObjects[index].posX = 0
+            Logic.screenObjects[index].posY = 0
+            Logic.screenObjects[index].sizeX = 0
+            Logic.screenObjects[index].sizeY = 0
+        }
+        
+        // Reset all strings
+        for index in 0 ..< Logic.numStrings {
+            Logic.strings[index] = ""
+        }
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCondition_Equal() throws {
+        
+        if let conditionEqual = Logic.conditionCommands[0x01]?.copy(),
+           conditionEqual.name == Logic.CommandName.condition_equal {
+            
+            // Given
+            let value: UInt8 = 5
+            let variableNum: UInt8 = 1
+            Logic.variables[Int(variableNum)] = value
+            
+            conditionEqual.data = [variableNum, value]
+            
+            // When
+            let result = conditionEqual.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Equal_V() throws {
+        
+        if let conditionEqualV = Logic.conditionCommands[0x02]?.copy(),
+           conditionEqualV.name == Logic.CommandName.condition_equal_v {
+            
+            // Given
+            let value1: UInt8 = 5
+            let variableNum1: UInt8 = 1
+            Logic.variables[Int(variableNum1)] = value1
+            
+            let value2: UInt8 = 5
+            let variableNum2: UInt8 = 2
+            Logic.variables[Int(variableNum2)] = value2
+            
+            conditionEqualV.data = [variableNum1, variableNum2]
+            
+            // When
+            let result = conditionEqualV.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Less() throws {
+        
+        if let conditionLess = Logic.conditionCommands[0x03]?.copy(),
+           conditionLess.name == Logic.CommandName.condition_less {
+            
+            // Given
+            let value: UInt8 = 5
+            let variableNum: UInt8 = 1
+            Logic.variables[Int(variableNum)] = value
+            
+            conditionLess.data = [variableNum, value + 1]
+            
+            // When
+            let result = conditionLess.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Less_V() throws {
+        
+        if let conditionLessV = Logic.conditionCommands[0x04]?.copy(),
+           conditionLessV.name == Logic.CommandName.condition_less_v {
+            
+            // Given
+            let value1: UInt8 = 5
+            let variableNum1: UInt8 = 1
+            Logic.variables[Int(variableNum1)] = value1
+            
+            let value2 = value1 + 1
+            let variableNum2: UInt8 = 2
+            Logic.variables[Int(variableNum2)] = value2
+            
+            conditionLessV.data = [variableNum1, variableNum2]
+            
+            // When
+            let result = conditionLessV.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Greater() throws {
+        
+        if let conditionGreater = Logic.conditionCommands[0x05]?.copy(),
+           conditionGreater.name == Logic.CommandName.condition_greater {
+            
+            // Given
+            let value: UInt8 = 5
+            let variableNum: UInt8 = 1
+            Logic.variables[Int(variableNum)] = value
+            
+            conditionGreater.data = [variableNum, value - 1]
+            
+            // When
+            let result = conditionGreater.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Greater_V() throws {
+        
+        if let conditionGreaterV = Logic.conditionCommands[0x06]?.copy(),
+           conditionGreaterV.name == Logic.CommandName.condition_greater_v {
+            
+            // Given
+            let value1: UInt8 = 5
+            let variableNum1: UInt8 = 1
+            Logic.variables[Int(variableNum1)] = value1
+            
+            let value2 = value1 - 1
+            let variableNum2: UInt8 = 2
+            Logic.variables[Int(variableNum2)] = value2
+            
+            conditionGreaterV.data = [variableNum1, variableNum2]
+            
+            // When
+            let result = conditionGreaterV.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Isset() throws {
+        
+        if let conditionIsset = Logic.conditionCommands[0x07]?.copy(),
+           conditionIsset.name == Logic.CommandName.condition_isset {
+            
+            // Given
+            let flagNum: UInt8 = 1
+            Logic.flags[Int(flagNum)] = true
+            
+            conditionIsset.data = [flagNum]
+            
+            // When
+            let result = conditionIsset.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    func testCondition_Isset_V() throws {
+        
+        if let conditionIssetV = Logic.conditionCommands[0x08]?.copy(),
+           conditionIssetV.name == Logic.CommandName.condition_isset_v {
+            
+            // Given
+            let variableNum: UInt8 = 1
+            Logic.variables[Int(variableNum)] = 1
+            
+            conditionIssetV.data = [variableNum]
+            
+            // When
+            let result = conditionIssetV.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if an objects position is in the bounding box given by the top-left and bottom-right coords
+    func testCondition_Position_Inside() throws {
+        
+        if let conditionPosition = Logic.conditionCommands[0x0B]?.copy(),
+           conditionPosition.name == Logic.CommandName.condition_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 5
+            object.posY = 5
+            
+            conditionPosition.data = [objectNum, 0, 0, 10, 10]
+            
+            // When
+            let result = conditionPosition.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if an objects position is not in the bounding box given by the top-left and bottom-right coords
+    func testCondition_Position_Outside() throws {
+        
+        if let conditionPosition = Logic.conditionCommands[0x0B]?.copy(),
+           conditionPosition.name == Logic.CommandName.condition_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 15
+            object.posY = 15
+            
+            conditionPosition.data = [objectNum, 0, 0, 10, 10]
+            
+            // When
+            let result = conditionPosition.evaluate()
+            
+            // Then
+            XCTAssertFalse(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Compare 2 strings to see if they are equal
+    func testCondition_CompareStrings_Simple() throws {
+        
+        if let conditionCompareStrings = Logic.conditionCommands[0x0F]?.copy(),
+           conditionCompareStrings.name == Logic.CommandName.condition_compare_strings {
+            
+            // Given
+            let stringNum1: UInt8 = 0
+            let stringNum2: UInt8 = 1
+            
+            Logic.strings[Int(stringNum1)] = "test"
+            Logic.strings[Int(stringNum2)] = "test"
+            
+            conditionCompareStrings.data = [stringNum1, stringNum2]
+            
+            // When
+            let result = conditionCompareStrings.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Compare 2 strings to see if they are equal, test if ignored characters are really ignored
+    func testCondition_CompareStrings_Ignore() throws {
+        
+        if let conditionCompareStrings = Logic.conditionCommands[0x0F]?.copy(),
+           conditionCompareStrings.name == Logic.CommandName.condition_compare_strings {
+            
+            // Given
+            let stringNum1: UInt8 = 0
+            let stringNum2: UInt8 = 1
+            
+            Logic.strings[Int(stringNum1)] = "te-st"
+            Logic.strings[Int(stringNum2)] = "tes!t;"
+            
+            conditionCompareStrings.data = [stringNum1, stringNum2]
+            
+            // When
+            let result = conditionCompareStrings.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Compare 2 strings to see if they are equal, test if ignored characters are really ignored
+    func testCondition_CompareStrings_Different() throws {
+        
+        if let conditionCompareStrings = Logic.conditionCommands[0x0F]?.copy(),
+           conditionCompareStrings.name == Logic.CommandName.condition_compare_strings {
+            
+            // Given
+            let stringNum1: UInt8 = 0
+            let stringNum2: UInt8 = 1
+            
+            Logic.strings[Int(stringNum1)] = "test"
+            Logic.strings[Int(stringNum2)] = "different"
+            
+            conditionCompareStrings.data = [stringNum1, stringNum2]
+            
+            // When
+            let result = conditionCompareStrings.evaluate()
+            
+            // Then
+            XCTAssertFalse(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if an object is entirely in (position and size) the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_ObjInBox_Inside() throws {
+        
+        if let conditionObjInBox = Logic.conditionCommands[0x10]?.copy(),
+           conditionObjInBox.name == Logic.CommandName.condition_obj_in_box {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 5
+            object.posY = 5
+            object.sizeX = 3
+            object.sizeY = 3
+            
+            conditionObjInBox.data = [objectNum, 0, 0, 10, 10]
+            
+            // When
+            let result = conditionObjInBox.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if an object is outside (position and size) the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_ObjInBox_Outside() throws {
+        
+        if let conditionObjInBox = Logic.conditionCommands[0x10]?.copy(),
+           conditionObjInBox.name == Logic.CommandName.condition_obj_in_box {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 5
+            object.posY = 5
+            object.sizeX = 6
+            object.sizeY = 6
+            
+            conditionObjInBox.data = [objectNum, 0, 0, 10, 10]
+            
+            // When
+            let result = conditionObjInBox.evaluate()
+            
+            // Then
+            XCTAssertFalse(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if the center of an object is in the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_CenterPosition_Inside() throws {
+        
+        if let conditionCenterPosition = Logic.conditionCommands[0x11]?.copy(),
+           conditionCenterPosition.name == Logic.CommandName.condition_center_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 0
+            object.posY = 0
+            object.sizeX = 8
+            object.sizeY = 8
+            
+            conditionCenterPosition.data = [objectNum, 0, 0, 5, 5]
+            
+            // When
+            let result = conditionCenterPosition.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if the center of an object is outside the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_CenterPosition_Outside() throws {
+        
+        if let conditionCenterPosition = Logic.conditionCommands[0x11]?.copy(),
+           conditionCenterPosition.name == Logic.CommandName.condition_center_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 0
+            object.posY = 0
+            object.sizeX = 8
+            object.sizeY = 8
+            
+            conditionCenterPosition.data = [objectNum, 5, 5, 8, 8]
+            
+            // When
+            let result = conditionCenterPosition.evaluate()
+            
+            // Then
+            XCTAssertFalse(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if the right side of an object is in the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_RightPosition_Inside() throws {
+        
+        if let conditionRightPosition = Logic.conditionCommands[0x12]?.copy(),
+           conditionRightPosition.name == Logic.CommandName.condition_right_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 0
+            object.posY = 8
+            object.sizeX = 8
+            object.sizeY = 8
+            
+            conditionRightPosition.data = [objectNum, 7, 7, 12, 12]
+            
+            // When
+            let result = conditionRightPosition.evaluate()
+            
+            // Then
+            XCTAssertTrue(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+    
+    // Test if the right side of an object is outside the bounding box given by
+    // the top-left and bottom-right coords
+    func testCondition_RightPosition_Outside() throws {
+        
+        if let conditionRightPosition = Logic.conditionCommands[0x12]?.copy(),
+           conditionRightPosition.name == Logic.CommandName.condition_right_position {
+            
+            // Given
+            let objectNum: UInt8 = 0
+            
+            let object = Logic.screenObjects[Int(objectNum)]
+            object.posX = 0
+            object.posY = 0
+            object.sizeX = 8
+            object.sizeY = 8
+            
+            conditionRightPosition.data = [objectNum, 2, 2, 7, 7]
+            
+            // When
+            let result = conditionRightPosition.evaluate()
+            
+            // Then
+            XCTAssertFalse(result)
+            
+        } else {
+            XCTFail()
+        }
+    }
+}
+
